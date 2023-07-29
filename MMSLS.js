5936c4fd37750861f97bf3956af5294b8576d3a9
confusq <78801788+confusq@users.noreply.github.com>
2023-07-29 17:14:40 +0800

    Update MMSLS.js

--- MMSLS.js
+++ MMSLS.js
@@ -7,7 +7,7 @@
 =====================请在下面编辑=====================
  */
 const MMSLS = [
-    "https://www.maimemo.com/share/page?uid=5390766&pid=acc56ebe038246e082b2519d11bcaeb9&tid=1c0dd5843c1ed292d7d03d93bda1f106",
+    "https://www.maimemo.com/share/page?uid=5390766&pid=6dc9bf4ffde7a017a5c8659c5f17929e&tid=3d83e2f0cdef3e6be89b0bdd818f5c11",
   ]
   /**
    * 生成随机数字
