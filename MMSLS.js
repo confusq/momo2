736625b66c88db82c84cab1b63e2656137f9e542
confusq <78801788+confusq@users.noreply.github.com>
2023-09-29 23:18:16 +0800

    Update MMSLS.js

--- MMSLS.js
+++ MMSLS.js
@@ -8,7 +8,7 @@
  */
 const MMSLS = [
 
-    "https://www.maimemo.com/share/page?uid=5390766&pid=5da1d4d693448e924d48a7ff0d113868&tid=0a3f8b7dd902fe82bda153f3f38185fa",
+    "https://www.maimemo.com/share/page?uid=5390766&pid=9af231acd6863ac9cc366a2f2e52ef36&tid=3996f8e7e28ca1239275bd88ccee4e8d",
   ]
   /**
    * 生成随机数字
